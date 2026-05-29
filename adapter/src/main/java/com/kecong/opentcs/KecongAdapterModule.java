d8aae2eea49d5bf1d95a9492013ca50cc499cc52
zhouzhipeng <18909246586@qq.com>
2026-05-29 09:34:51 +0800

    fix: 移除 opentcs-kernel 依赖，改用 Guice AbstractModule

--- adapter/src/main/java/com/kecong/opentcs/KecongAdapterModule.java
+++ adapter/src/main/java/com/kecong/opentcs/KecongAdapterModule.java
@@ -1,19 +1,20 @@
 package com.kecong.opentcs;
 
-import org.opentcs.customizations.kernel.KernelInjectionModule;
+import com.google.inject.AbstractModule;
+import org.opentcs.drivers.vehicle.management.VehicleCommAdapterFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Guice module that registers the KecongCommAdapterFactory with the openTCS kernel.
  */
-public class KecongAdapterModule extends KernelInjectionModule {
+public class KecongAdapterModule extends AbstractModule {
 
     private static final Logger LOG = LoggerFactory.getLogger(KecongAdapterModule.class);
 
     @Override
     protected void configure() {
         LOG.info("Registering KecongCommAdapterFactory via Guice module");
-        vehicleCommAdaptersBinder().addBinding().to(KecongCommAdapterFactory.class);
+        bind(VehicleCommAdapterFactory.class).to(KecongCommAdapterFactory.class);
     }
 }
