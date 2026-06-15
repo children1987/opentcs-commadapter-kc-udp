6920a44f1e2593eed60a2fc6f9b1d45f131873a0
zhouzhipeng <18909246586@qq.com>
2026-06-15 13:29:51 +0800

    chore: update paths after moving to commadapters/ directory

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 import zipfile, os
 
-adapter = 'E:/workspace/opentcs/opentcs-commadapter-kc-udp'
+adapter = 'E:/workspace/opentcs/commadapters/opentcs-commadapter-kc-udp'
 build = os.path.join(adapter, 'build', 'classes_v2')
 out = os.path.join(adapter, 'build', 'kecong-opentcs-adapter-1.0.0.jar')
 
